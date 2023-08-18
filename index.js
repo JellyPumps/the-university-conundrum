4c8a215cf127e7685f709109eba841095889919c
Sarthak Rai <88274478+JellyPumps@users.noreply.github.com>
2023-08-18 10:43:13 +0530

    some fun randomization

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const toggles = document.querySelectorAll('.switch input');
-    
+const selectedToggles = []; // Array to store selected switches
+
 toggles.forEach(toggle => {
     toggle.addEventListener('change', () => {
-        if (document.querySelectorAll('.switch input:checked').length > 2) {
-            toggle.checked = false;
+        if (toggle.checked) {
+            if (selectedToggles.length >= 2) {
+                const randomIndex = Math.floor(Math.random() * selectedToggles.length);
+                const toggleToToggleOff = selectedToggles[randomIndex];
+                toggleToToggleOff.checked = false;
+
+                // Remove the toggled-off switch from the selectedToggles array
+                selectedToggles.splice(randomIndex, 1);
+            }
+            selectedToggles.push(toggle); // Add the newly selected toggle
+        } else {
+            const indexToRemove = selectedToggles.indexOf(toggle);
+            if (indexToRemove !== -1) {
+                selectedToggles.splice(indexToRemove, 1); // Remove the deselected toggle
+            }
         }
     });
 });
